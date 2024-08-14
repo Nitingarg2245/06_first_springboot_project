f0ac49d5db2b0c3c959e225f0b725edae12d740a
Nitin Garg <ngarg2260@gmail.com>
2024-08-14 15:33:57 +0530

    Initial commit

---
+++ src/main/java/com/nitin/service/Bike.java
@@ -0,0 +1,11 @@
+package com.nitin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Bike  {
+    public Bike()
+    {
+        System.out.println("bike object created");
+    }
+}
---
+++ src/main/java/com/nitin/service/Car.java
@@ -0,0 +1,11 @@
+package com.nitin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Car  {
+    public Car() {
+        System.out.println("car object created");
+    }
+
+}
